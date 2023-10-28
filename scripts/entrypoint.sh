49ae6ab53938484f0d2d7ffefa6c10eca3349726
gardais <147820580+gardais@users.noreply.github.com>
2023-10-28 19:10:35 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -20,29 +20,19 @@ elif [ "$INPUT_REPORTER" == 'github-pr-review' ] ||
      [ "$INPUT_REPORTER" == 'github-pr-check' ]; then
   # We will need this token for `reviewdog` to work:
   export REVIEWDOG_GITHUB_API_TOKEN="$GITHUB_TOKEN"
-  echo "In loop --exit-zero"
   # Running special version of `flake8` to match the `reviewdog` format:
   output=$(flake8 --exit-zero $INPUT_PATH --append-config='/action-config.cfg')
   echo "$output" | reviewdog -f=pep8 -reporter="$INPUT_REPORTER" -level=error
   # `reviewdog` does not fail with any status code, so we have to get dirty:
   status=$(test "$output" = ''; echo $?)
-  status=0
 else
   output="Invalid action reporter specified: $INPUT_REPORTER"
   status=1
 fi
 echo '================================'
-echo "$output"
-echo "$status"
-echo '================================'
-# Sets the output variable for Github Action API:
-# See: https://help.github.com/en/articles/development-tools-for-github-action
-echo '================================'
-echo
 
 # Fail the build in case status code is not 0:
 if [ "$status" != 0 ]; then
-  echo "$output"
   echo "Process failed with the status code: $status"
   exit "$status"
 fi
